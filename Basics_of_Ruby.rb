35072baffaea57b258b1a52d78c97d7cc2bf53ba
Amar K <71604396+Amark19@users.noreply.github.com>
2022-07-02 01:03:47 +0530

    Till the functions all the basics are covered

---
+++ Basics_of_Ruby.rb
@@ -0,0 +1,82 @@
+#Basics of Ruby
+
+###Taking input from user
+
+print "what is your name "
+name = gets.chomp
+print "my name is #{name}"
+name ="amar"
+x = (name.include?"am") ? 1:0
+puts x
+
+##Arrays
+
+x = Array.new(10)
+x = [1,2,4]
+puts x
+puts x[1]
+puts x[10]
+# puts x.fetch(10)
+x = x.push(10)
+puts x
+
+##Hash
+
+dict = {"hi"=>4,'hello'=>4}
+dict1 = {"hi":4,'hello':4}
+puts dict.size
+puts dict1[:"hello"]
+
+##If-Else
+
+if 2>4
+  puts "hello"
+elsif 5>5
+  puts "hii"
+else
+  puts "Bye"
+end
+
+##Loops
+##For Loops
+
+for i in 99..100
+puts i
+end
+
+x = [1,2,3,4,5]
+for i in x
+puts i
+end
+
+##While Loop
+
+i=10
+while i>0 do
+    puts i
+    i-=1
+end
+
+##Until
+
+i=0
+until i==10
+    puts i
+    i+=1
+end
+
+##redo
+
+i=10
+while i>0 do
+    puts i
+    i-=1
+    redo if i==0
+end
+
+###functions
+
+def hello (name,roll_no)
+    puts "hello my name is #{name} and my roll no is #{roll_no}"
+end
+hello("amar",23)
